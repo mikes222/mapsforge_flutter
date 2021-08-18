d438a591ea4644d8dc1680000c4164d8068e4e8c
David Lange <53982746+Davomat@users.noreply.github.com>
2021-08-18 16:45:26 +0200

    updated necessary dependencies, introduced named parameters to mapfile data

--- indoor_example/lib/map-file-data.dart
+++ indoor_example/lib/map-file-data.dart
@@ -1,24 +1,25 @@
 import 'dart:io';
+import 'package:flutter/cupertino.dart';
 import 'package:path_provider/path_provider.dart';
 
 class MapFileData {
   final String url;
   final String fileName;
-  final String name;
+  final String displayedName;
   final double initialPositionLat;
   final double initialPositionLong;
   final int initialZoomLevel;
 
-  MapFileData(
-    this.url,
-    this.fileName,
-    this.name,
-    this.initialPositionLat,
-    this.initialPositionLong,
-    this.initialZoomLevel
-  );
+  MapFileData({
+    @required this.url,
+    @required this.fileName,
+    @required this.displayedName,
+    @required this.initialPositionLat,
+    @required this.initialPositionLong,
+    this.initialZoomLevel = 18,
+  });
 
-  Future<String> getLocalFilePath () async {
+  Future<String> getLocalFilePath() async {
     Directory dir = await getApplicationDocumentsDirectory();
     return dir.path + "/" + fileName;
   }
