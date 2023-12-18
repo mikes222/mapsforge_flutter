a942eac365b60aa492defde67c07f704998d9477
pamtbaau <pamtbaau@gmail.com>
2023-12-18 08:28:43 +0100

    Upgrade dummy test of mapsforge_storage

--- mapsforge_storage/test/mapsforge_storage_method_channel_test.dart
+++ mapsforge_storage/test/mapsforge_storage_method_channel_test.dart
@@ -3,19 +3,22 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:mapsforge_storage/mapsforge_storage_method_channel.dart';
 
 void main() {
-  MethodChannelMapsforgeStorage platform = MethodChannelMapsforgeStorage();
-  const MethodChannel channel = MethodChannel('mapsforge_storage');
-
   TestWidgetsFlutterBinding.ensureInitialized();
 
+  MethodChannelMapsforgeStorage platform = MethodChannelMapsforgeStorage();
+  const MethodChannel channel = MethodChannel('magweg');
+
   setUp(() {
-    channel.setMockMethodCallHandler((MethodCall methodCall) async {
-      return '42';
-    });
+    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger.setMockMethodCallHandler(
+      channel,
+      (MethodCall methodCall) async {
+        return '42';
+      },
+    );
   });
 
   tearDown(() {
-    channel.setMockMethodCallHandler(null);
+    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger.setMockMethodCallHandler(channel, null);
   });
 
   test('getPlatformVersion', () async {
